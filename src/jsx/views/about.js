221bcd857e2902cbd1462ac679a74967e0e1d74d
Achhunna M <achhunna@users.noreply.github.com>
2023-04-13 13:13:49 -0700

    remove stitch fix

--- src/jsx/views/about.js
+++ src/jsx/views/about.js
@@ -13,9 +13,6 @@ class About extends React.Component {
                             front-end engineer with a passion for learning new technologies and building products that bring meaningful change
                         </li>
                         <li>
-                            currently at <a href="https://algorithms-tour.stitchfix.com/" target="_blank">Stitch Fix Algorithms</a> doing neat things with data
-                        </li>
-                        <li>
                             some <Link to="/uses">things</Link> I use on a daily basis
                         </li>
                         <li>
