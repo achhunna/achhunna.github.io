62a3cc2ae6043dd7e8e21f420ed5874ecf639468
Achhunna Mali <achhunna@gmail.com>
2023-06-16 21:53:14 -0700

    basePath to ''

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 // @ts-check
-
-const isProd = process.env.NODE_ENV === 'production'
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/' : '',
+  basePath: '',
   experimental: {
     appDir: true,
   },
