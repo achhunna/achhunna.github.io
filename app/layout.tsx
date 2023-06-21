10c65d600bc167ed1cd3288b2149ae286913250e
Achhunna Mali <achhunna@gmail.com>
2023-06-20 21:20:25 -0700

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './global.css'
+import Script from 'next/script'
 
 export default function RootLayout({
   children,
@@ -8,6 +9,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-MVPNFMLX0K"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MVPNFMLX0K');
+  `,
+        }}
+      />
     </html>
   )
 }
