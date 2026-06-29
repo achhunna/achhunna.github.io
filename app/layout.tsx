82f10ae7fa5cc8c6c462a17e9f8d1d1ef470e100
achhunna <achhunna@gmail.com>
2026-06-29 13:17:46 -0700

    remove google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import Script from 'next/script'
 
 declare global {
   interface Window {
-    amplitude: any;
-    sessionReplay: any;
+    amplitude: any
+    sessionReplay: any
   }
 }
 
@@ -21,29 +21,14 @@ export default function RootLayout({
         <Script
           src="https://cdn.amplitude.com/script/1ddaaad51e2ff25a9d5d4366e64c735d.js"
           onReady={() => {
-            window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));
-            window.amplitude.init('1ddaaad51e2ff25a9d5d4366e64c735d', {"autocapture":{"elementInteractions":true, "pageViews": false}});
+            window.amplitude.add(window.sessionReplay.plugin({ sampleRate: 1 }))
+            window.amplitude.init('1ddaaad51e2ff25a9d5d4366e64c735d', {
+              autocapture: { elementInteractions: true, pageViews: false },
+            })
           }}
         />
       </head>
       <body>{children}</body>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-MVPNFMLX0K"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-MVPNFMLX0K');
-  `,
-        }}
-      />
     </html>
   )
 }
